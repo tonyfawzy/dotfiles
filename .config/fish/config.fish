2b5715aa7f24d7d08b13a4c1af3f4f3d89c59954
tonyfawzy <tonyfawzy@outlook.com>
2023-09-22 09:00:01 +0300

    new file:   .config/fish/config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+#    _                    
+#   | |_ ___  _ __  _   _ 
+#   | __/ _ \| '_ \| | | |
+#   | || (_) | | | | |_| |  https://github.com/tonyfawzy
+#    \__\___/|_| |_|\__, |
+#                   |___/ 
+
+set TERM "xterm-256color"
+
+#Aliases
+alias vim='nvim'
+
+#Poweroff, reboot
+alias spf='sudo systemctl poweroff'
+alias srb='sudo systemctl reboot'
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
